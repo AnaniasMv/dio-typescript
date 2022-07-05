e57da15b6b6926febeeaff40bb9d0f3febd64467
AnaniasMv <ananiasmatheus22@outlook.com>
2022-07-05 12:30:03 -0300

    Initial commit

---
+++ src/desafios/desafio3.ts
@@ -0,0 +1,29 @@
+// O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
+
+let botaoAtualizar = document.getElementById('atualizar-saldo')!;
+let botaoLimpar = document.getElementById('limpar-saldo')!;
+let soma = document.getElementById('soma')! as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo')!;
+let saldoTotal = 0
+
+function somarAoSaldo(soma:number) {
+    campoSaldo.innerHTML += soma;
+    campoSaldo.innerHTML = saldoTotal.toString();
+    limparCampoSoma();
+}
+
+function limparCampoSoma() {
+    soma.value = "";
+}
+
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(soma.value));
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
